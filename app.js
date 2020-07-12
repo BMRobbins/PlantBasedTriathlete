60c4b2a59b8bd0ce0fb97b23d82de9d0f89d34ce
Brian <brobbins1984@gmail.com>
2020-07-12 07:48:13 -0700

    Adding navigation and server endpoints

--- app.js
+++ app.js
@@ -12,8 +12,46 @@ app.use(express.static("public"));
 
 //mongoose.connect("mongodb://localhost:27017l/todolistDB", {useUnifiedTopology: true , useNewUrlParser:true, useFindAndModify: false});
 
+
+// Home endPoints
 app.get("/", function(req, res) {
     res.render("home");
+});
+
+
+//Blog endPoints
+app.get("/blog", function(req, res) {
+    res.render("blog");
+});
+
+app.get("/blog:blogId", function(req, res) {
+    res.render("blog");
+});
+
+
+// Recipes endPoints 
+app.get("/recipes", function(req, res) {
+    res.render("recipes");
+});
+
+app.get("/recipes/breakfast", function(req, res) {
+    res.render("recipes");
+});
+
+app.get("/recipes/lunch", function(req, res) {
+    res.render("recipes");
+});
+
+app.get("/recipes/dinner", function(req, res) {
+    res.render("recipes");
+});
+
+app.get("/recipes/desert", function(req, res) {
+    res.render("recipes");
+});
+
+app.get("/recipes/recipe:recipeId", function(req, res) {
+    res.render("recipes");
 });
 
 app.listen(3000, function() {
